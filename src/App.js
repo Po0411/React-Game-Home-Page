8648aa6a723f93c69a4e7b6aa62cf38cf90c01df
조하랑 <wawa111027@gmail.com>
2024-10-31 11:49:43 +0900

    코드 정리 - 1.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, { useState } from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Gtec from './Components/Gtec/Gtec'
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Gtec from './Components/Gtec/Gtec';
 import Gstar from './Components/Gstar/Gstar';
 import bannerImage from './assets/VANNER.png';
+import './App.css'; // 새로운 스타일을 위한 CSS 파일
 
 const App = () => {
   return (
-    <div className='container'>
+    <div className="container">
       <Navbar />
-      <img
-        src={bannerImage}
-        alt="Banner"
-        style={{ width: '70%', height: 'auto', display: 'block', margin: '0 auto' }}
-      />
-      <div style={{ margin: '60px 0' }}> {/* img와 Gtec 사이의 공백 */}
+      <img src={bannerImage} alt="Banner" className="banner-image" />
+      <div className="section-spacing">
         <Gtec />
       </div>
       <Gstar />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import './Navbar.css'
-import logo_game from '../../assets/logo.png'
+import React from 'react';
+import './Navbar.css';
+import logo_game from '../../assets/logo.png';
 
 const Navbar = () => {
+  const navItems = [
+    { label: '학과안내', link: 'https://example.com' },
+    { label: '캐릭터', link: 'https://example.com' },
+    { label: '3D모델링', link: 'https://example.com' },
+    { label: '게임작품', link: 'https://example.com' },
+  ];
+
   return (
-    <div className='navbar'>
-      <img src={logo_game} alt='' className='logo'/>
+    <div className="navbar">
+      <img src={logo_game} alt="Logo" className="logo" />
       <ul>
-        <li>
-          <a href="https://example.com" target="_blank" rel="noopener noreferrer" className="fontTest">학과안내</a>
-        </li>
-        <li className="fontTest">캐릭터</li>
-        <li className="fontTest">3D모델링</li>
-        <li className="fontTest">게임작품</li>
+        {navItems.map((item, index) => (
+          <li key={index}>
+            <a href={item.link} target="_blank" rel="noopener noreferrer" className="fontTest">
+              {item.label}
+            </a>
+          </li>
+        ))}
       </ul>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
