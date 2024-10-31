d43fc22a05619fcfe74d6c50e131fbfca58261ee
조하랑 <wawa111027@gmail.com>
2024-10-31 13:33:20 +0900

    일단 집가서한다...(학년 클래스 연결 실패)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Gtec from './Components/Gtec/Gtec';
 import Gstar from './Components/Gstar/Gstar';
+import class_3 from './Components/class/class3/class_3';
 import bannerImage from './assets/VANNER.png';
-import './App.css'; // 새로운 스타일을 위한 CSS 파일
+import './App.css';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Gtec />
       </div>
       <Gstar />
+      <class_3 />
     </div>
   );
 };
